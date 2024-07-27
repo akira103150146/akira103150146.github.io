0583913d11153f5ad11240edeb462a17dd2c0b82
Akira <sunroad0418@gmail.com>
2024-07-27 16:30:33 +0800

    login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+
+function Login() {
+    const [account, setAccount] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e: any) => {
+        console.log('Account', account);
+        console.log('Password', password);
+    }
+
+    return <div>
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+            <label>Account:</label>
+            <input type="text" value={account} onChange={(e) => setAccount(e.target.value)}></input>
+            <label>Password:</label>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+            <button type="submit"></button>
+        </form>
+    </div>
+}
+
+
+
+export default Login;
