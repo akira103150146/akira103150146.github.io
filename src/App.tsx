0dba62e8023369c8c62bf4e1bba08ab7df26ec5f
Akira <sunroad0418@gmail.com>
2024-07-27 16:36:21 +0800

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import Login from "./pages/Login";
+
 function App() {
   return (
-    <div className="App">
-        Hello world      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={Layout()}>
+          <Route index element={Login()}/>
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
